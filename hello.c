b36478ec15c848240d192f382fedcf5a3a5ea00a
fmstereo42 <postmaster@florence-maraninchi.fr>
2015-11-18 16:49:41 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 // Nov 18, 2015  This is my first file.
+// with int reads and print
 
 #include <stdio.h>
 void main () {
+	int d;
 	printf ("Hello World, I'm Florence\n");
+	printf ("Give me a number:");
+	scanf ("%d", &d);
+	printf ("your number is: %d\n", d+42);
 }
