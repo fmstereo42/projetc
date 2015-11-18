257aa150fc30f641f2148e13d56ebcae138aa8ab
Florence Maraninchi <florence.maraninchi@imag.fr>
2015-11-18 17:07:24 +0100

    strings dans cette branche

--- hello.c
+++ hello.c
@@ -1,11 +1,11 @@
 // Nov 18, 2015  This is my first file.
-// with int reads and print
+// with string reads and print
 
 #include <stdio.h>
 void main () {
-	int d;
+	char str[12];
 	printf ("Hello World, I'm Florence\n");
-	printf ("Give me a number:");
-	scanf ("%d", &d);
-	printf ("your number is: %d\n", d+42);
+	printf ("Give me a string:");
+	scanf ("%s", str);
+	printf ("your string is: %s%d\n", s, 42);
 }
